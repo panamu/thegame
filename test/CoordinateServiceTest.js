08130a7d101b2a0d72c78d581c21f4a29eedaba5
Padmini Namuduri <pnamuduri@expedia.com>
2019-08-15 11:55:27 -0700

    Update CoordinateServiceTest.js

--- test/CoordinateServiceTest.js
+++ test/CoordinateServiceTest.js
@@ -17,11 +17,11 @@ describe('CoordinateService', () => {
 
         player.changeDirection(Direction.LEFT);
         CoordinateService.movePlayer(player);
-        let expectedSegments = [new Coordinate(6, 1),
-                                new Coordinate(5, 1),
-                                new Coordinate(4, 1),
-                                new Coordinate(3, 1),
-                                new Coordinate(2, 1)];
+        let expectedSegments = [new Coordinate(6, 2),
+                                new Coordinate(7, 3),
+                                new Coordinate(8, 4),
+                                new Coordinate(9, 5),
+                                new Coordinate(10, 6)];
         assert.deepEqual(player.getSegments(), expectedSegments, 'Player did not move right as expected');
 
         player.changeDirection(Direction.UP);
